3e8edb77f7e2ff3ca1a30ea6b3579ab5b6da260a
Ingmar Rieger <git@irieger.net>
2021-02-27 17:53:37 +0100

    Input validation and individual zoom control
    Add image data input validation.
    Add zoom control per image instead of one shared zoom factor.
    Fix unintended reuse of last width/height in the image viewer window.

--- multi_image.py
+++ multi_image.py
@@ -2,10 +2,10 @@
 # Only 8bit RGB-images work. Data can be both float and uint
 # Only one image can be displayed at a time. Python interpreter paused while Window is open
 # Zoom is fixed by Zoom-slider from 1% to 200%
-# TODO: - multiple images displayed simultaniously. Multithreading / thread
+# TODO: - [done] multiple images displayed simultaniously. Multithreading / thread
 #       - mouse zoom and scroll
-#       - 10bit image display
-#       - grayscale Array 2D to 3D  [:,:,none]
+#       - [done] 10bit image display
+#       - [done] grayscale Array 2D to 3D  [:,:,none]
 
 # Dependencies numpy, Imgui[glfw], OpenGL use !pip install
 import numpy as np
@@ -15,6 +15,8 @@ import OpenGL.GL as gl
 
 import imgui
 from imgui.integrations.glfw import GlfwRenderer
+
+from sys import platform
 
 from multiprocessing import Process, current_process, Queue
 
@@ -36,10 +38,44 @@ def zoom_factor(z):
 # display can except multiple images args* -> what modifications in main
 # same zoom? easier...
 
+
+def createImageTexture(image):
+    texture_id = gl.glGenTextures(1)
+    gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, 1)
+    gl.glBindTexture(gl.GL_TEXTURE_2D, texture_id)
+    gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP_TO_EDGE)
+    gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_EDGE)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST)
+
+    width = image.shape[1]
+    height = image.shape[0]
+    tex_data_converted = None
+
+    if len(image.shape) == 2:
+        image = np.repeat(image[..., None], 3, axis=2)
+    elif len(image.shape) == 3 and image.shape[2] == 1:
+        image = np.repeat(image, 3, axis=2)
+    elif len(image.shape) == 3 and image.shape[2] == 4:
+        image = image[:, :, :3]
+
+    if 'int' in str(image.dtype):
+        tex_data_converted = np.float32(image) / np.iinfo(image.dtype).max
+    elif str(image.dtype).startswith('float'):
+        tex_data_converted = np.float32(image)
+    else:
+        print("Image array must have a float or integer type.", image.dtype, 'is not supported')
+        raise TypeError
+
+    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB32F, width, height, 0, gl.GL_RGB,
+        gl.GL_FLOAT, tex_data_converted)
+    # gl.glGenerateMipmap(gl.GL_TEXTURE_2D)
+
+    return (texture_id, width, height)
+
+
 def imguiThreadWorker(queue, window_name = 'PyImageViewer'):
     running = True
-
-    zoom = 100
 
     imgui.create_context()
     window = impl_glfw_init(window_name=window_name)
@@ -47,8 +83,7 @@ def imguiThreadWorker(queue, window_name = 'PyImageViewer'):
 
     textures = []  # List of texture IDs + labels
     
-    loop_count = 0
-    img_cnt = 0
+    img_cnt = 1
 
     while (not glfw.window_should_close(window)) and running:
         # Poll queue
@@ -58,34 +93,14 @@ def imguiThreadWorker(queue, window_name = 'PyImageViewer'):
                 running = False
             elif type(entry) is tuple:
                 print('New image: ', entry[0].shape, entry[0].dtype, entry[1])
-                texture_id = gl.glGenTextures(1)
-                gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, 1)
-                gl.glBindTexture(gl.GL_TEXTURE_2D, texture_id)
-                gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP_TO_EDGE)
-                gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_EDGE)
-                gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
-                gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST)
+                texture_id, width, height = createImageTexture(entry[0])
 
-                texture_data = entry[0]
-                width = texture_data.shape[1]
-                height = texture_data.shape[1]
-                tex_data_converted = None
-                dt = texture_data.dtype
-                dt_str = str(dt)
-
-                if 'int' in dt_str:
-                    tex_data_converted = np.float32(texture_data) / np.iinfo(dt).max
-                elif dt_str.startswith('float'):
-                    tex_data_converted = np.float32(texture_data)
-                else:
-                    print("Image array must have a float or integer type. Not:", dt)
-                    raise TypeError
-
-                gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB32F, width, height, 0, gl.GL_RGB,
-                    gl.GL_FLOAT, tex_data_converted)
-                # gl.glGenerateMipmap(gl.GL_TEXTURE_2D)
-
-                textures.append((texture_id, 'Image' + str(img_cnt) + ': ' + entry[1]))
+                textures.append((
+                    texture_id,
+                    'Image' + str(img_cnt) + ': ' + entry[1],
+                    width,
+                    height))
+                img_cnt += 1
         except:
             pass
 
@@ -93,30 +108,20 @@ def imguiThreadWorker(queue, window_name = 'PyImageViewer'):
         impl.process_inputs()
         imgui.new_frame()
         
-        # Images
-        # if loop_count == 0:
-        #     print('Num textures: ', len(textures))
-        # loop_count = (loop_count + 1) % 150
+        # Display all current images
         for image_idx in range(len(textures)):
-            # if image_idx > 0:
-            #     print('Image IDX > 0')
+            zoom = 100
             tex = textures[image_idx]
             imgui.begin(tex[1], True, flags=imgui.WINDOW_HORIZONTAL_SCROLLING_BAR)
-            imgui.image(tex[0], float(width*zoom_factor(zoom)), float(height*zoom_factor(zoom)))
+
+            changed, zoom = imgui.slider_int("Zoom 10% - 1000%", zoom, min_value=1, max_value=200, 
+                                             format="{}".format(str(int(zoom_factor(zoom)*100)) ) )
+            imgui.same_line()
+            if imgui.button("Zoom 100%", 80, 40):
+                zoom = 100
+
+            imgui.image(tex[0], float(tex[2]*zoom_factor(zoom)), float(tex[3]*zoom_factor(zoom)))
             imgui.end()
-
-        # Slider
-        imgui.begin("Zoom", True)
-        imgui.set_window_size(250, 150, imgui.FIRST_USE_EVER)
-        changed, zoom = imgui.slider_int("Zoom 10% - 1000%", zoom, min_value=1, max_value=200, 
-                                        format="{}".format(str(int(zoom_factor(zoom)*100)) ) )
-        imgui.text("Changed: %s" % (changed))
-        # reset Zoom
-        z_reset =  imgui.button("Zoom 100%", 80, 40)
-        imgui.end()
-
-        if z_reset:
-            zoom = 100
 
         gl.glClearColor(.25, .25, .25, 1)
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
@@ -146,103 +151,26 @@ class ImageViewer:
         
 
     def display(self, image, label = ''):
-        self.queue.put((image, label))
-
-
-# shared_iv = None
-
-# def display(image, label = ''):
-#     '''
-#     Display Image via Imgui
-#     '''
-#     global shared_iv
-#     if shared_iv is None:
-#         shared_iv = ImageViewer()
-#     shared_iv.display(image, label)
-    
-
-
-# def zoom_factor(z):
-#     z = z/100
-#     if z >= 1:
-#         return z ** 3.322
-#     elif z < 1:
-#         return z*0.9 + 0.1
-#     else:
-#         return 1
-
-# def check_dim(a):
-#     # check if array if not return Error
-#     # check what dimension 2D, 3D? All else Error (1D?)
-#     # if 3D how many values in 3. Dimension
-#     # do: modify arrays
-#     # return modified array
-#     pass
-
-# # main function that generates the window and image output    
-# def main(width, height, texture_data, texture_id, zoom, dt):
-#     imgui.create_context()
-#     window = impl_glfw_init()
-#     impl = GlfwRenderer(window)
-
-#     texture_id = gl.glGenTextures(1)
-#     gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, 1)
-#     gl.glBindTexture(gl.GL_TEXTURE_2D, texture_id)
-#     gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP_TO_EDGE)
-#     gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP_TO_EDGE)
-#     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
-#     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST)
-
-#     # Make sure the supplied image is converted to float32 and normalized if input data is
-#     # in an integer format.
-#     tex_data_converted = None
-#     dt_str = str(dt)
-#     if 'int' in dt_str:
-#         tex_data_converted = np.float32(texture_data) / np.iinfo(dt).max
-#     elif dt_str.startswith('float'):
-#         tex_data_converted = np.float32(texture_data)
-#     else:
-#         print("Image array must have a float or integer type. Not:", dt)
-#         raise TypeError
-
-#     gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB32F, width, height, 0, gl.GL_RGB,
-#         gl.GL_FLOAT, tex_data_converted)
-    
-#     gl.glGenerateMipmap(gl.GL_TEXTURE_2D)
-
-
-#     while not glfw.window_should_close(window):
-#         glfw.poll_events()
-#         impl.process_inputs()
-#         imgui.new_frame()
+        if not isinstance(label, str):
+            print('Invalid label type, not a string!')
+            return
         
-#         # Image
-#         imgui.begin("Image", True, flags=imgui.WINDOW_HORIZONTAL_SCROLLING_BAR)
-#         imgui.image(texture_id, float(width*zoom_factor(zoom)), float(height*zoom_factor(zoom)))
-#         imgui.end()
-
-#         # Slider
-#         imgui.begin("Zoom", True)
-#         imgui.set_window_size(250, 150, imgui.FIRST_USE_EVER)
-#         changed, zoom = imgui.slider_int("Zoom 10% - 1000%", zoom, min_value=1, max_value=200, 
-#                                         format="{}".format(str(int(zoom_factor(zoom)*100)) ) )
-#         imgui.text("Changed: %s" % (changed))
-#         # reset Zoom
-#         z_reset =  imgui.button("Zoom 100%", 80, 40)
-#         imgui.end()
-
-#         if z_reset:
-#             zoom = 100
-
-#         gl.glClearColor(.25, .25, .25, 1)
-#         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-
-#         imgui.render()
-#         impl.render(imgui.get_draw_data())
-#         glfw.swap_buffers(window)
-
-#     impl.shutdown()
-#     glfw.terminate()
+        if isinstance(image, np.ndarray):
+            dt = str(image.dtype)
+            if not (dt.startswith('float') or 'int' in dt):
+                print('Invalid numpy array type')
+                return
+        else:
+            print('Image is not a numpy array!')
+            return
+        
+        if not len(image.shape) in [2, 3]:
+            print('Image needs to be a 2D numpy array (grayscale) or 3D (3 or 4 channel, RGB(a))')
+            return
+        if len(image.shape) == 3 and not image.shape[2] in [1, 3, 4]:
+            print('Invalid image size in 3rd dimension!')
+            return
+        self.queue.put((image, label))
 
 
 def impl_glfw_init(window_name):
@@ -256,12 +184,13 @@ def impl_glfw_init(window_name):
     glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 0)
 
     # OS X supports only forward-compatible core profiles from 3.2
-    # # Original setup for mac compatibility. Doesn't allow checking bit
-    # # depth on Linux at least thus commented out
-    # glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
-    # glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-    # glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-    # glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
+    # Original setup for mac compatibility. Doesn't allow checking bit
+    # depth on Linux so it is only checked on darwin (macos)
+    if platform == 'darwin':
+        glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+        glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+        glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
 
     # Create a windowed mode window and its OpenGL context
     window = glfw.create_window(
@@ -269,12 +198,13 @@ def impl_glfw_init(window_name):
     )
     glfw.make_context_current(window)
 
-    # For debugging, just to check if 10, 10, 10, 2 rendering surface or 8, 8, 8, 8
-    redBits = gl.glGetIntegerv(gl.GL_RED_BITS)
-    greenBits = gl.glGetIntegerv(gl.GL_GREEN_BITS)
-    blueBits = gl.glGetIntegerv(gl.GL_BLUE_BITS)
-    alphaBits = gl.glGetIntegerv(gl.GL_ALPHA_BITS)
-    print('opengl bit depths [RGBA]: ', redBits, greenBits, blueBits, alphaBits)
+    if platform != 'darwin':
+        # For debugging, just to check if 10, 10, 10, 2 rendering surface or 8, 8, 8, 8
+        redBits = gl.glGetIntegerv(gl.GL_RED_BITS)
+        greenBits = gl.glGetIntegerv(gl.GL_GREEN_BITS)
+        blueBits = gl.glGetIntegerv(gl.GL_BLUE_BITS)
+        alphaBits = gl.glGetIntegerv(gl.GL_ALPHA_BITS)
+        print('opengl bit depths [RGBA]: ', redBits, greenBits, blueBits, alphaBits)
 
     if not window:
         glfw.terminate()
@@ -282,6 +212,3 @@ def impl_glfw_init(window_name):
         exit(1)
 
     return window
-
-# # if __name__ == "__main__":
-# #         main()
